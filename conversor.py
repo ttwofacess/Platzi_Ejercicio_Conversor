8c33e19c460e842e3af178179c02aa733a5b50cc
ttwofacess <ttwofacess@gmail.com>
2022-02-06 21:06:33 +0100

    Primer paso agregando dolar blue

--- conversor.py
+++ conversor.py
@@ -2,8 +2,8 @@ menu = """
 Bienvenido al conversor de monedas
 
 1 - Pesos colombianos
-2 - Pesos agentinos
-3 - Pesos mexicanos
+2 - Pesos argentinos
+3 - Pesos argentinos a Dolar Blue
 
 Elige una opcion: """
 
@@ -16,7 +16,7 @@ if opcion == 1:
     dolares = pesos / valor_dolar
     dolares = round(dolares, 2)
     dolares = str(dolares)
-    print("Tienes $" + dolares + " dolares")
+    print("Tienes $" + dolares + " Dolares")
 elif opcion == 2:
     pesos = input("Cuantos pesos tienes?: ")
     pesos = float(pesos)
@@ -24,15 +24,15 @@ elif opcion == 2:
     dolares = pesos / valor_dolar
     #dolares = round(dolares, 2)
     dolares = str(dolares)
-    print("Tienes $" + dolares + " dolares")
+    print("Tienes $" + dolares + " Dolares")
 elif opcion == 3:
     pesos = input("Cuantos pesos tienes?: ")
     pesos = float(pesos)
-    valor_dolar = 24
+    valor_dolar = 214
     dolares = pesos / valor_dolar
     dolares = round(dolares, 2)
     dolares = str(dolares)
-    print("Tienes $" + dolares + " dolares")
+    print("Tienes $" + dolares + " Dolares blue")
 else:
     print('Ingresa una opcion correcta por favor')
 
